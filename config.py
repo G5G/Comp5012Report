3961bbfb7c254740d815db42f9634e90f2ea2131
moein fahmide <moeinfahmide@moeins-Air.lan>
2024-04-15 20:26:12 +0100

    more simple crossover and mutation operators

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ FRONTIER_PREFIX = 'portef'
 
 # Define algorithm parameters
 ALGORITHM_PARAMS = {
-    "POP_SIZE": 500,
-    "NUM_GENERATIONS": 300,
+    "POP_SIZE": 1000,
+    "NUM_GENERATIONS": 50,
     "CROSSOVER_RATE" : 0.9,
     'mutation_rate': 0.2,
     'mutation_shift': 0.05
