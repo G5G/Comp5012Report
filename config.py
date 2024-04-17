fe3910ee588fa085e4294453c15807f10e484133
vil <123lietuvis5@gmail.com>
2024-04-17 19:29:00 +0100

    Updated the config file
    Updated the best parameters based on the random search

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ NO_OF_FILES = 1
 NO_OF_TRIALS = 50
 # Define algorithm parameters
 ALGORITHM_PARAMS = {
-    "POP_SIZE": 100,
-    "NUM_GENERATIONS": 50,
-    "CROSSOVER_RATE" : 0.2,
-    'mutation_rate': 0.2,
-    'mutation_shift': 0.05,
-    "ELITISM_COUNT": 3,  # Number of elites to retain each generation, CONSIDERING POP SIZE
+    "POP_SIZE": 250,
+    "NUM_GENERATIONS": 250,
+    "CROSSOVER_RATE" : 0.5,
+    'mutation_rate': 0.4,
+    'mutation_shift': 0.02,
+    "ELITISM_COUNT": 20,  # Number of elites to retain each generation, CONSIDERING POP SIZE
 
-    "crossover_type": "uniform_crossover",  # Options: 'uniform', 'sbx'
+    "crossover_type": "sbx_crossover",  # Options: 'uniform', 'sbx'
     "mutation_type": "gaussian_mutation", # Options: 'gaussian', 'polynomial'
 
     # adjust if sbx is choosen for crossover
